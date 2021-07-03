9b261b8de14b08e8c8977feb3db867dd1961f3d6
Patryk Mejnartowicz <patryk_mej@o2.pl>
2021-07-03 23:33:08 +0200

    Making models

--- charity/models.py
+++ charity/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Institution(models.Model):
+    TYPE_CHOICES = (
+        ("fundacja", "fundacja"),
+        ("organizacja pozarządowa", "organizacja pozarządowa"),
+        ("zbiórka lokalna", "zbiórka lokalna")
+    )
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=128)
+    type = models.CharField(max_length=64, choices=TYPE_CHOICES, default="fundacja")
+    categories = models.ManyToManyField(Category)
