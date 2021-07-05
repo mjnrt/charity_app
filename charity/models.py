6042f5d614d424def8af72112f66e98a2c26fd56
Patryk Mejnartowicz <patryk_mej@o2.pl>
2021-07-05 21:28:40 +0200

    Model donation and hrefs configurations

--- charity/models.py
+++ charity/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -15,3 +16,17 @@ class Institution(models.Model):
     description = models.CharField(max_length=128)
     type = models.CharField(max_length=64, choices=TYPE_CHOICES, default="fundacja")
     categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ManyToManyField(Institution)
+    address = models.CharField(max_length=64)
+    phone_number = models.CharField(max_length=20)
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=20)
+    pickup_date = models.DateField()
+    pickup_time = models.TimeField()
+    pickup_comment = models.TextField(max_length=240)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
